73ed9f5ee730b8428d11e02822a533c80d9c37e6
hientv <hientranvan27@gmail.com>
2023-10-17 11:45:02 +0700

    Init src

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,6 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoController } from './modules/todo/todo.controller';
-import { TodoModule } from './modules/todo/todo.module';
-import { TodoService } from './modules/todo/todo.service';
-import { UserController } from './modules/user/user.controller';
-import { UserModule } from './modules/user/user.module';
-import { UserService } from './modules/user/user.service';
-import { FeedbackController } from './modules/feedback/feedback.controller';
-import { FeedbackService } from './modules/feedback/feedback.service';
-import { FeedbackModule } from './modules/feedback/feedback.module';
-import { Todo } from './modules/todo/todo.entity';
-import { Feedback } from './modules/feedback/feedback.entity';
-import { User } from './modules/user/user.entity';
 
 @Module({
   imports: [
@@ -29,24 +17,18 @@ import { User } from './modules/user/user.entity';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      entities: [Todo, Feedback, User],
+      entities: [],
       logging: ['error', 'warn', 'log'],
       synchronize: true,
     }),
-    TodoModule,
-    UserModule,
-    FeedbackModule,
   ],
-  controllers: [AppController, TodoController, UserController, FeedbackController],
+  controllers: [AppController,],
   providers: [
     AppService,
     {
       provide: APP_INTERCEPTOR,
       useClass: ClassSerializerInterceptor,
     },
-    TodoService,
-    UserService,
-    FeedbackService,
   ],
 })
 export class AppModule { }
